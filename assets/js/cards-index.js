92568be77995c807df19349ab2ff9a3d657282e3
Luis-Felipe-N <luisj2felipe09@gmail.com>
2021-08-04 18:06:23 -0300

    add two projets/ add skill react

--- assets/js/cards-index.js
+++ assets/js/cards-index.js
@@ -3,25 +3,25 @@ import criarCards from './criar-cards.js'
 // Criar card page index
 const tdProjetos = [
     {   
-        title: 'Calculara APP',
-        link: 'https://github.com/Luis-Felipe-N/calculadora',
-        text: 'Calculadora APP é um desafio proposto pelo <a target="_blank" rel="noopener noreferrer" href="https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29">Front-end Mentor</a>.',
-        image: 'https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/bz3taijsat3rhsocodym.jpg',
-        technologies: ['scss', 'html', 'js']
-    },
-    {   
         title: 'REST Countries',
         link: 'https://rest-countries-luis.netlify.app/',
         text: 'Site que mostra mais de 200 países, com opções de fitragem e pesquisa.',
         image: 'https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/yhq5ihanseyinzwblaw1.jpg',
         technologies: ['js', 'css', 'html']
     },
-    {   
-        title: 'Gerador de Bordas',
-        link: 'https://border-radius-generator-luis.netlify.app/',
-        text: 'No Gerador de bordas você vê em tempo real O formato em radius e pega o código pronto.',
-        image: '/assets/img/ft-border-radius-generador.png',
-        technologies: ['css', 'html', 'js']
+    {
+        title: 'Let me ask',
+        link: 'https://letmeask-f1b55.web.app/',
+        text: 'Let me ask foi um feito no <a target="_blank" rel="noopener noreferrer"  href="https://nextlevelweek.com/pre-nlw"> Next Level Week </a> onde aprendemos a criar uma plataforma de comentários.',
+        image: 'https://github.com/Luis-Felipe-N/letmeask-1/raw/master/.github/cover.svg',
+        technologies: ['scss', 'html', 'react']
+    },
+    {
+        title: 'iFun',
+        link: 'https://ifun-65e7a.firebaseapp.com/',
+        text: 'iFun é um uma rede social de meme. Projeto criado para reforça o que aprendi no <a target="_blank" rel="noopener noreferrer"  href="https://nextlevelweek.com/pre-nlw"><span style="color: #866EE6; font-weight: 700">#NLW</span></a>.',
+        image: 'https://github.com/Luis-Felipe-N/ifun/raw/main/src/assets/image/image-example.svg',
+        technologies: ['scss', 'html', 'react']
     }
 
 ]
--- assets/js/cards-projetos.js
+++ assets/js/cards-projetos.js
@@ -92,6 +92,20 @@ const tdProjetos = [
         text: 'Time Now é uma landing page simples construido com <span class="color-sass c-sass">scss</span>. Desafio proposto por <a target="_blank" rel="noopener noreferrer" href="https://www.codewell.cc/challenges/608d9a7d747bad001532bd70">Codewell.cc</a>.',
         image: 'https://codewell-storage-bucket.s3.us-east-2.amazonaws.com/Time_Now_Slider_Thumbnail_min_713c17672e.jpg',
         technologies: ['scss', 'html', 'js']
+    },
+    {
+        title: 'Let me ask',
+        link: 'https://letmeask-f1b55.web.app/',
+        text: 'Let me ask foi um feito no <a target="_blank" rel="noopener noreferrer"  href="https://letmeask-f1b55.web.app/"> Next Level Week </a> onde aprendemos a criar uma plataforma de comentários.',
+        image: 'https://github.com/Luis-Felipe-N/letmeask-1/raw/master/.github/cover.svg',
+        technologies: ['scss', 'html', 'react']
+    },
+    {
+        title: 'iFun',
+        link: 'https://ifun-65e7a.firebaseapp.com/',
+        text: 'iFun é um uma rede social de meme. Projeto criado para reforça o que aprendi no <span style="color: #866EE6; font-weight: 700">#NLW</span>.',
+        image: 'https://github.com/Luis-Felipe-N/ifun/raw/main/src/assets/image/image-example.svg',
+        technologies: ['scss', 'html', 'react']
     }
 ]
 
@@ -100,7 +114,8 @@ const cores = {
     html: '#E44B27',
     css: '#254CDE',
     python: '#366F9E',
-    sass:'#cf649a'
+    sass:'#cf649a',
+    React: "#61DAFB"
 };
 
 const quantCards = tdProjetos.length
